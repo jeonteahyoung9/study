485337de5b985f15dc25f430c68a5483be539e24
전태형 <wjsxogud5543@gmail.com>
2017-05-14 21:39:54 +0900

    stream

--- test/test/main.cpp
+++ test/test/main.cpp
@@ -1,8 +1,42 @@
 //first main function
 
 #include <stdio.h>
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+class my_variables
+{
+public:
+	int a;
+	int b;
+
+	void printa()
+	{
+		printf("%d %d\n", a, b);
+	}
+};
+
+std::ostream &operator << (std::ostream &stream, my_variables obj)
+{
+	stream << obj.a << " " << obj.b << std::endl;
+
+	return stream;
+}
 
 int main()
 {
-	printf("Hello, World!");
+	my_variables my_variables;
+
+	my_variables.a = 7;
+	my_variables.b = 5;
+	my_variables.printa();
+
+	printf("hello, world!\n");
+
+	cout << "this is c++ style!" << endl;
+
+	std::ofstream ofile("style.txt");
+	ofile << my_variables << std::endl;
 }
